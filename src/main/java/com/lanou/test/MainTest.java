6409c9abbf4bad5f24869130984b8a804a8acfc8
dllo <1198475006@qq.com>
2017-09-28 16:17:21 +0800

    在MainTest文件中添加一行有争议的代码123

--- src/main/java/com/lanou/test/MainTest.java
+++ src/main/java/com/lanou/test/MainTest.java
@@ -6,9 +6,8 @@ package com.lanou.test;
 public class MainTest {
     public static void main(String[] args) {
         System.out.println("Lemo tree");
-        Integer a = 20 ;
-        a++;
-        System.out.println(a);
+       Integer a = 20;
+        System.out.println(a++);
         System.out.println("困的一比");
     }
 }
