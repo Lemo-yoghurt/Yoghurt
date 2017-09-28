a265127273235475de45dcb01286e86f81f7995a
dllo <1198475006@qq.com>
2017-09-28 16:14:03 +0800

    在MainTest文件中添加一行有争议的代码

--- src/main/java/com/lanou/test/MainTest.java
+++ src/main/java/com/lanou/test/MainTest.java
@@ -6,6 +6,9 @@ package com.lanou.test;
 public class MainTest {
     public static void main(String[] args) {
         System.out.println("Lemo tree");
+        Integer a = 20 ;
+        a++;
+        System.out.println(a);
         System.out.println("困的一比");
     }
 }
